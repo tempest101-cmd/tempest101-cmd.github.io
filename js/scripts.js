b0c4926a928e3c6d8fd682578609b0dfc20642e4
tempest101-cmd <gamingabs3@yahoo.com>
2025-02-12 21:41:46 +0200

    Update scripts.js
    new id

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbydPqPZn5GLdyDGskHPR2_y9o5E3wckPWSvyWbBXaFnl4dmBH9U73t9vbmewibEKeE4Qw/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzMHywN7qx6fXJG2AN4pAmPxlkkKqXEfK8H6yuAJwMnrVwO3bzPh5MRw72M24PDpNcxiw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
